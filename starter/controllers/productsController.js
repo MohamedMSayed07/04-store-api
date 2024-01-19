47293b36ad36f7b070a5ffcffda51bcdcd53eefb
MohamedM.Sayed <mohamedm.sayed07@gmail.com>
2024-01-20 01:41:36 +0200

    Finished

--- starter/controllers/productsController.js
+++ starter/controllers/productsController.js
@@ -1,7 +1,7 @@
 const Product = require('../models/productModel');
 
 module.exports.getAllProducts = async (req, res, next) => {
-  const {featured, company, name, sort} = req.query;
+  const {featured, company, name, sort, fields, numericFilters} = req.query;
   const queryObject = {};
 
   if(featured)
@@ -12,9 +12,28 @@ module.exports.getAllProducts = async (req, res, next) => {
 
   if(name)
     queryObject.name = {$regex: name, $options: 'i'};
+
+  if(numericFilters) {
+    const operatorMap = {
+      '>':'$gt',
+      '>=':'$gte',
+      '=':'$eq',
+      '<':'$lt',
+      '<=':'$lte'
+    };
+    const regex = /\b(<|<=|=|>|>=)\b/g;
+    let filters = numericFilters.replace(regex, (match) => `-${operatorMap[match]}-`);
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach(item => {
+      const [field, operator, value] = item.split('-');
+      if(options.includes(field)) {
+        queryObject[field] = {[operator]: Number(value)};
+      }
+    });
+  }
   
   let result = Product.find(queryObject);
-  
+  // Sort
   if(sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
@@ -23,8 +42,19 @@ module.exports.getAllProducts = async (req, res, next) => {
     result = result.sort('createdAt');
   }
 
+  if(fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+const page = Number(req.query.page) || 1;
+const limit = Number(req.query.limit) || 10;
+const skip = (page - 1) * limit;
+
+result = result.skip(skip).limit(limit);
+
   const products = await result;
-  res.status(200).json({products});
+  res.status(200).json({nbHits: products.length, products});
     // throw new Error('testing async errors');
 };
 
